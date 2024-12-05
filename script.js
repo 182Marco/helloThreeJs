19363523064df6869871436d6196042f6b80378e
Milza <marco.milza@capgemini.com>
2024-12-05 22:22:00 +0100

    colore changed

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-console.log(THREE);
-
-// instanciate the scene
+// Instantiate the scene
 const scene = new THREE.Scene();
 
-// initialize mesh
-
+// Initialize mesh
 const mesh = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "pink" })
+  new THREE.MeshBasicMaterial({ color: "#8be9fd" })
 );
 
-// add mesch in the scende
+// Add mesh to the scene
 scene.add(mesh);
 
 const aspect = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-//initialize che camera
-const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height); // default values of near is 1 and for far is 2000
+
+// Initialize the camera
+const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height); // Default values: near=1, far=2000
 camera.position.z = 3;
 camera.position.x = 1;
-//initialize che canvas
+camera.position.y = 1;
 
+// Initialize the canvas
 const canvas = document.querySelector(".draw");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(aspect.width, aspect.height);
+
+// Set the background color to #282a36
+renderer.setClearColor("#282a36");
+
+// Render the scene
 renderer.render(scene, camera);
