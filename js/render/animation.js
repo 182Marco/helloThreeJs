5ad0db3abb32be7b0ee22fe5a0eef5de0ee5be6d
Milza <marco.milza@capgemini.com>
2024-12-10 00:01:54 +0100

    animate a 16th of turn per second

--- js/render/animation.js
+++ js/render/animation.js
@@ -1,5 +1,5 @@
 const animate = () => {
-  cube1.rotation.x = clock.getElapsedTime();
+  cube1.rotation.x = clock.getElapsedTime() * (Math.PI / 8);
   renderer.render(scene, camera);
   window.requestAnimationFrame(animate);
 };
